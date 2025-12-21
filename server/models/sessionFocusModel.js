45d59d6e3b333399d20806e8d5755e7177d6c4f3
NourElhoda1 <bakmshalsbata@gmail.com>
2025-12-21 01:03:57 +0100

    base de donnees

---
+++ server/models/sessionFocusModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const mongoosePagination = require('mongoose-paginate-v2') ;
+
+const SchemaSessionFocus = new mongoose.Schema({
+  utilisateur: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Utilisateur',
+    required: true
+  },
+  tache: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tache',
+  },
+  heureDebut: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  heureFin: {
+    type: Date
+  },
+  dureeEnMinutes: {
+    type: Number,
+    required: true
+  },
+  statut: {
+    type: String,
+    enum: ['termine', 'interrompu', 'abandonne'], 
+    default: 'termine'
+  },
+  nombreDistractions: {
+    type: Number, 
+    default: 0 
+  },
+  niveauConcentration: {
+    type: Number,
+    min: 1,
+    max: 5
+  }
+}, { timestamps: true });
+
+const mongoosePagination = require('mongoose-paginate-v2');
+SchemaSessionFocus.plugin(mongoosePagination);
+const sessionFocusModel = mongoose.model('SessionFocus', SchemaSessionFocus);
+module.exports = sessionFocusModel;
